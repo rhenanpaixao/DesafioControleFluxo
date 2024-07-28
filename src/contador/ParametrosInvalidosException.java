bac18ebe14d30dfc8d09f384704e689cf2323894
Rhenan Paixao <rhenandpl@gmail.com>
2024-07-28 15:59:50 -0400

    Commit inicial do projeto DesafioControleFluxo

---
+++ src/contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package contador;
+
+public class ParametrosInvalidosException extends Exception {
+	 public ParametrosInvalidosException() {
+	        super("O segundo parâmetro deve ser maior que o primeiro");
+	    }
+}
